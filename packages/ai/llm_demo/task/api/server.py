25c46f1dedb9f85b0d3758337e401dfcd61c91e6
shuhei <shuhei.nishikawa_111@icloud.com>
2025-11-22 23:03:40 +0900

    fix: リクエストの受け取り方を修正

--- packages/ai/llm_demo/task/api/server.py
+++ packages/ai/llm_demo/task/api/server.py
@@ -5,14 +5,19 @@ from llm_demo.task.service.question_task import question_task_service
 # APIルータの定義
 router = APIRouter()
 
+# リクエストモデル
+class QuestionTaskRequest(BaseModel):
+    prompt: str
+
+# レスポンスモデル
 class QuestionTaskResponse(BaseModel):
     """question_taskのレスポンス"""
     answer: str
 
 @router.post("/", response_model=QuestionTaskResponse)
-async def question_task(prompt: str):
+async def question_task(request: QuestionTaskRequest):
     """タスクについて質問する"""
-    response = question_task_service.excute(prompt=prompt)
+    response = question_task_service.excute(prompt=request.prompt)
     return QuestionTaskResponse(
         answer=response.answer
     )
