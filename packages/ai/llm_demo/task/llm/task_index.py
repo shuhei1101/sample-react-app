ff19b24aee31452c86cfd6cc1adf63eb869e6ce2
shuhei <shuhei.nishikawa_111@icloud.com>
2025-11-23 18:37:46 +0900

    feat: ローディング状態の追加。aiの回答を調整。

--- packages/ai/llm_demo/task/llm/task_index.py
+++ packages/ai/llm_demo/task/llm/task_index.py
@@ -1,4 +1,5 @@
 from llm_demo.task.cloud_storage.task_doc_storage import task_doc_storage
+from llama_index.llms.openai import OpenAI
 
 # タスクインデックス
 task_index = task_doc_storage.load_index()
@@ -6,4 +7,10 @@ task_index = task_doc_storage.load_index()
 if task_index == None:
     raise Exception("タスクインデックスが存在しませんでした。")
 # タスククエリエンジン
-task_query_engine = task_index.as_query_engine()
+task_query_engine = task_index.as_query_engine(
+    llm=OpenAI(
+        model="gpt-4o-mini",
+        temperature=0.5,
+        system_prompt="すべての回答は日本語で返してください。"
+    )
+)
--- packages/web/app/(task)/tasks/question/page.tsx
+++ packages/web/app/(task)/tasks/question/page.tsx
@@ -1,18 +1,18 @@
 "use client"
 
-import { Suspense, useState } from "react";
-import { AuthorizedPageLayout } from "@/app/(auth)/_components/AuthorizedPageLayout";
-import { ActionIcon, Button, Container, Input, Paper, Text, Textarea } from "@mantine/core";
-import { useRouter } from "next/navigation";
-import { IconSend } from "@tabler/icons-react";
-import { useSendMessage } from "./_hooks/useTaskQuestion";
+import { Suspense, useState } from "react"
+import { AuthorizedPageLayout } from "@/app/(auth)/_components/AuthorizedPageLayout"
+import { ActionIcon, Button, Container, Input, Loader, Paper, Text, Textarea } from "@mantine/core"
+import { useRouter } from "next/navigation"
+import { IconSend } from "@tabler/icons-react"
+import { useSendMessage } from "./_hooks/useTaskQuestion"
 
 
 function QuestionContent() {
-  const router = useRouter();
+  const router = useRouter()
 
   /** 入力欄の状態 */
-  const [prompt, setPrompt] = useState<string>("");
+  const [prompt, setPrompt] = useState<string>("")
   /** チャット */
   const [messages, setMessages] = useState<{
     text: string,
@@ -20,27 +20,37 @@ function QuestionContent() {
   }[]>([{
     text: "こんにちは。質問を入力してください。",
     type: "ai"
-  }]);
+  }])
+
+  /** ローディング状態 */
+  const [loading, setLoading] = useState(false)
 
   /** ハンドラ */
   const { handleSend } = useSendMessage()
 
   /** 送信ボタン押下時の処理 */
   const onClickSend = async (prompt: string) => {
+    // プロンプトが空の場合、処理を終了する
+    if (prompt.trim() === "") return
     try {
-    // 入力内容をメッセージに追加する
-    setMessages(prev => [...prev, 
-      {text: prompt, type: "user"},
-    ])
-    // 入力フィールドをクリアする
-    setPrompt("")
-    // 入力内容を送信する
-    const data = await handleSend(prompt)
-    setMessages(prev => [...prev, 
-      {text: data!.answer, type: "ai"},
-    ])
+      // ローディング中にする
+      setLoading(true)
+      // 入力内容をメッセージに追加する
+      setMessages(prev => [...prev, 
+        {text: prompt, type: "user"},
+      ])
+      // 入力フィールドをクリアする
+      setPrompt("")
+      // 入力内容を送信する
+      const data = await handleSend(prompt)
+      setMessages(prev => [...prev, 
+        {text: data!.answer, type: "ai"},
+      ])
     } catch (error) {
       console.log(error)
+    } finally {
+      // ローディング状態を解除する
+      setLoading(false)
     }
   }
 
@@ -52,26 +62,34 @@ function QuestionContent() {
           {messages.map((message, index) => {
             return (
               <div key={index} className={message.type === "ai" ? "flex justify-start" : "flex justify-end"}>
-                <Paper
-                  shadow="xs"
-                  radius="xl"
-                  p="sm"
-                  className="max-w-2/3 min-h-0"
-                  bg={message.type === "ai" ? undefined : "#ADFF2F"}
-                >
+                <Paper shadow="xs" radius="xl" p="sm" className="max-w-2/3 min-h-0" bg={message.type === "ai" ? undefined : "#ADFF2F"} >
                   <Text>{message.text}</Text>
                 </Paper>
               </div>
             )
           })}
+          {/* ローディング中の表示 */}
+          {loading &&
+            <div className={"flex justify-start"}>
+              <Paper shadow="xs" radius="xl" p="sm" className="max-w-2/3 min-h-0" >
+                <Loader color="blue" size="xs" type="dots" />
+              </Paper>
+            </div>
+          }
         </div>
         <div className="m-5" />
         {/* チャット入力欄 */}
         <div className="flex items-center gap-2">
           {/* 入力フィールド */}
-          <Textarea className="w-full" placeholder="質問を入力してください。" autosize minRows={1} maxRows={4} 
+          <Textarea className="w-full" placeholder="例: 現在登録中のタスクを教えて下さい。" autosize minRows={1} maxRows={4} 
             value={prompt}
             onChange={(event) => setPrompt(event.currentTarget.value)}
+            onKeyDown={(event) => {
+              if ((event.ctrlKey || event.metaKey) && event.key === "Enter") {
+                event.preventDefault() // 改行を抑制する
+                onClickSend(prompt) // 送信する
+              }
+            }}
           />
           {/* 送信ボタン */}
           <ActionIcon onClick={()=>{
